2f32eb77c8f334ce27ae1d2cf333cf948999a1fa
Luis Ortega <a.ortega.lepe@gmail.com>
2024-01-01 14:47:31 -0300

    Exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,17 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = () => {
     return (
@@ -28,9 +34,9 @@ const App = () => {
   const Content = () => {
     return (
       <div>
-        <Part part={part1} exercise={exercises1} />
-        <Part part={part2} exercise={exercises2} />
-        <Part part={part3} exercise={exercises3} />
+        <Part part={part1.name} exercise={part1.exercises} />
+        <Part part={part2.name} exercise={part2.exercises} />
+        <Part part={part3.name} exercise={part3.exercises} />
       </div>
     )
   }
@@ -38,7 +44,7 @@ const App = () => {
   const Total = () => {
     return (
       <div>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
       </div>
     )
   }
